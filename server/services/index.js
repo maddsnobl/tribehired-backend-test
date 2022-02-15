8bd14f3d43245e3fbd3cc3152b648003aafcc9f1
Maddie Nobel <madonna.nobel@pickupp.io>
2022-02-15 13:20:38 +0800

    Set up services index

---
+++ server/services/index.js
@@ -0,0 +1,8 @@
+/*
+  In production, this will be a microservice
+  for the posts/comments
+*/
+
+module.exports.getComments = 'https://jsonplaceholder.typicode.com/comments'
+module.exports.getPost = 'https://jsonplaceholder.typicode.com/posts/'
+module.exports.getAllPosts = 'https://jsonplaceholder.typicode.com/posts'
